145e48424bb0b8760ba3b6ee9adaa77fa188f452
Carlos Duclos <darkzeratul64@gmail.com>
2021-02-19 15:42:48 -0500

    Initial Commit

---
+++ SparkDigital/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SparkDigital
+//
+//  Created by Carlos Duclos on 19/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
